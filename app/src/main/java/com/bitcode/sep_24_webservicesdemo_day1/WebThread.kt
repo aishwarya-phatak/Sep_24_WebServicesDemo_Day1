c2fdcaaa0c5b7d779ec57704ad6811bdf43f6e60
aishwarya <ashp17star@gmail.com>
2024-11-18 17:35:02 +0530

    Initial commit

---
+++ app/src/main/java/com/bitcode/sep_24_webservicesdemo_day1/WebThread.kt
@@ -0,0 +1,55 @@
+package com.bitcode.sep_24_webservicesdemo_day1
+
+import android.util.Log
+import org.json.JSONArray
+import org.json.JSONObject
+import java.net.HttpURLConnection
+import java.net.URL
+
+class WebThread : Thread() {
+
+    override fun run() {
+        super.run()
+
+        val url = URL("https://api.slingacademy.com/v1/sample-data/photos?offset=5&limit=20")
+        var httpURLConnection = url.openConnection() as HttpURLConnection
+        httpURLConnection.connect()
+        Log.e("tag", "----------------------------------------")
+        Log.e("tag", httpURLConnection.requestMethod)
+        Log.e("tag", httpURLConnection.responseMessage)
+        Log.e("tag", httpURLConnection.responseCode.toString())
+        Log.e("tag", "----------------------------------------")
+
+        var inStream = httpURLConnection.inputStream
+        Log.e("tag", "----------------------------------------")
+        Log.e("tag", inStream.toString())
+
+
+        var buffer = StringBuffer()
+        var data = ByteArray(1024 * 1)
+        var count = 0
+
+        count = inStream.read(data)
+        while (count != -1){
+            buffer.append(String(data, 0 , count))
+            count = inStream.read(data)
+        }
+        inStream.close()
+        Log.e("tag", "----------------------------------------")
+        Log.e("tag",buffer.toString())
+
+
+        var responseObject = JSONObject(buffer.toString())
+        var successValue = responseObject.getBoolean("success")
+        var totalPhotos = responseObject.getInt("total_photos")
+        var message = responseObject.getString("message")
+        var offset = responseObject.getInt("offset")
+        var limit = responseObject.getInt("limit")
+
+
+        var photosArray = responseObject.getJSONArray("photos")
+        for (i in photosArray.length()-1){
+
+        }
+    }
+}
